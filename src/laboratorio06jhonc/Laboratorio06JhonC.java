d9dbfb3a0066fdbf0544cb8ba09f106d0ca2b867
JhonOrtegaE <142622512+JhonOrtegaE@users.noreply.github.com>
2023-10-05 23:50:26 -0500

    Add files via upload

---
+++ src/laboratorio06jhonc/Laboratorio06JhonC.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package laboratorio06jhonc;
+
+/**
+ *
+ * @author USER
+ */
+public class Laboratorio06JhonC {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GestorCurso curso = new GestorCurso();
+        curso.setLocationRelativeTo(null);
+        curso.setVisible(true);
+    }
+    
+}
